2e52bfca6cb6ec802b0d4cf0cc6833bcfbee6439
Jules Sorensen <julessorens@gmail.com>
2023-06-25 11:35:58 +0200

    setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  clearScreen: false,
+  server: {
+    strictPort: true,
+    port: 8082,
+  },
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    target: ['es2021', 'chrome100', 'safari13'],
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
   plugins: [vue()],
 })
