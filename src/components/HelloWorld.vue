781c1aa2b316e87dbc981309bb3863bbf76b6945
Jules Sorensen <julessorens@gmail.com>
2023-06-25 12:01:36 +0200

    first tauri function

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,31 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { invoke } from '@tauri-apps/api';
 
-defineProps<{ msg: string }>()
+defineProps<{
+  msg: string,
+}>()
+
+const helloMessage = ref('')
+
+async function hello() {
+  const result: any = await invoke('hello', {
+    name: 'World'
+  });
+  helloMessage.value = result.toString();
+}
 
 const count = ref(0)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+  <h2 v-if="helloMessage != ''">{{ helloMessage }}</h2>
 
   <div class="card">
+    <button type="button" @click="hello()">Hello</button>
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
